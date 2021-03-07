78ebc5e0be7f2195a1eae893a6ae44bf5f5aa1c7
Giovanni Benato <benatogiovanni@gmail.com>
2021-03-07 21:54:59 +0100

    Added optional ROOT link; added notes to readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Playground
+
+CMake options:
+
+-DCMAKE_DISABLE_FIND_PACKAGE_ROOT=true --> Disables ROOT
+-DROOT_DIR=... --> Manually pass ROOT installation dir
--- apps/ParameterGenerator.cxx
+++ apps/ParameterGenerator.cxx
@@ -1,14 +1,32 @@
+#include "Global.hh"
 #include "Log.hh"
 #include "Parameter.hh"
+#include "Random.hh"
+
+#ifdef HAVE_ROOT
+#include "TH1D.h"
+#endif
 
 int main()
 {
+
     int verbosity = 4;
     Log::OpenLog( verbosity );
+
+    //Global* global = Global::GetInstance();
+    Global::Initialize();
+    
 
     Parameter* par = new Parameter( "TestPar", 0., 10., "cm" );
     par->SetPrior( Parameter::PriorType::kGaussian, 5., 1. );
 
+    Random rdm;
+
+    #ifdef HAVE_ROOT
+	    TH1D* histo = new TH1D();
+	    Log::OutDebug( "Created histo" );
+    #endif
+    
     Log::OutSummary( "Everything done." );
     
     return 0;
